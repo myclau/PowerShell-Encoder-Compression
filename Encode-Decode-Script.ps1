d1136b52649ce4b54bde108e48cb6ba727686201
Matthias Lau <myclau@gmail.com>
2018-11-01 12:12:36 +0800

    Remove example section to make it clean

--- Encode-Decode-Script.ps1
+++ Encode-Decode-Script.ps1
@@ -57,19 +57,3 @@ foreach($char in $charArray){
     return $DecodeArray -Join ''
  }
  
- #example
- $text = " `$u=5;`$l=70;
- if((Get-Ciminstance -Namespace root/WMI -ClassName WmiMonitorBrightness).CurrentBrightness -ge 50){
-    `$i=`$u
- }else{
-    `$i=`$l
- }
- (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,`$i)
- "
- 
-
- $encodeScript= Encode-Script "$text"
- write-host $encodeScript
- $originalText =  Decode-script "$encodeScript"
- write-host $originalText
- Decode-run-script "$encodeScript"
